150b65e2be4d106b77dad4b3486cbe393e69425b
Robert Hamilton <rhamilton1415@gmail.com>
2020-02-23 11:12:28 +0000

    feat(UIViewExtensions): Add makeEqualToSuperview method

---
+++ stoverflow/utilities/UIViewExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewExtensions.swift
+//  stoverflow
+//
+//  Created by Robert Hamilton on 22/02/2020.
+//  Copyright © 2020 Robert Hamilton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    /// Adds constraints to make a view equal to its superview
+    /// - Parameter insets: An UIEdgeInsets instance to configure the spacing to the superview. By default, all values
+    ///  are zero
+    func makeEqualToSuperview(insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)) {
+        superview?.addConstraints([
+            NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: superview, attribute: .leftMargin, multiplier: 1, constant: insets.left),
+            NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: superview, attribute: .rightMargin, multiplier: 1, constant: insets.right),
+            NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: superview, attribute: .topMargin, multiplier: 1, constant: insets.top),
+            NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: superview, attribute: .bottomMargin, multiplier: 1, constant: insets.bottom)
+        ])
+    }
+}
